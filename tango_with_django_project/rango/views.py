2749c5abd6129a66b3391dd99a7d93f48f39912b
Mckenzie Morrison <2915568mstudent.gla.ac.uk>
2025-02-14 12:09:52 +0000

    conteext lol

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def index(request):
     context_dict = {'boldmessage':'Crunchy, creamy, cookie, candie, cupcake!'}
-    return render(request, 'rango/index.html', conteext=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
